1f2b5bf1b88a71c3a1e764f69b541d6b9be92d72
Robin Millette <robin@millette.info>
2019-09-21 12:32:10 -0400

    Link to edit page.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
-import Link from 'next/link'
-import 'isomorphic-unfetch'
-import MDX from 'mdx-runtime-slim'
-import { MDXProvider } from '@mdx-js/react'
+import Link from "next/link"
+import "isomorphic-unfetch"
+import MDX from "mdx-runtime-slim"
+import { MDXProvider } from "@mdx-js/react"
 
 const FrontPage = ({ MDXContent }) => (
   <MDXProvider>
     <div>
-      <p>Hello <Link href="/p2"><a>Earth</a></Link></p>
+      <p>
+        Hello{" "}
+        <Link href="/p2">
+          <a>Earth</a>
+        </Link>
+      </p>
       <MDX>{MDXContent}</MDX>
+      <p>
+        <Link href="/ed">
+          <a>Edit</a>
+        </Link>
+      </p>
     </div>
   </MDXProvider>
 )
 
 FrontPage.getInitialProps = async ({ req }) => {
-  // console.log('FrontPage.getInitialProps') // , a, b, c)
-  const res = await fetch('http://localhost:3000/c1.txt')
+  console.log(!req, typeof req)
+  const res = await fetch("http://localhost:3000/c1.mdx")
   const c = await res.text()
-  // console.log('CCC', c)
-  // return { MDXContent: '### more stuff' }
   return { MDXContent: c }
 }
 
 export default FrontPage
- 

5edb66d76f5f62ddd167e30f2a888dd43077502c
Robin Millette <robin@millette.info>
2019-09-22 16:35:04 -0400

    Offer to create new page.

--- pages/custom/[page].js
+++ pages/custom/[page].js
@@ -1,3 +1,4 @@
+// import Error from 'next/error'
 import Link from "next/link"
 import MDXRuntime from "mdx-runtime-slim"
 import { MDXProvider } from "@mdx-js/react"
@@ -9,38 +10,62 @@ import Clock from "../../components/clock"
 
 const components = { Clock }
 
-const FrontPage = ({ MDXContent, page }) => (
-  <MDXProvider components={components}>
-    <div>
-      <p>
-        Hello{" "}
-        <Link href="/p2">
-          <a>Earth</a>
-        </Link>
-      </p>
-      <MDXRuntime>{MDXContent}</MDXRuntime>
-      <p>
-        <Link href={`/custom/ed/${page}`}>
-          <a>Edit</a>
-        </Link>
-      </p>
-    </div>
-  </MDXProvider>
+const AnError = ({ statusCode, page }) => (
+  <div>
+    <code>{page}</code> doesn't exist.{" "}
+    <Link href={`/custom/ed/${page}`}>
+      <a>Create it?</a>
+    </Link>
+  </div>
 )
 
-FrontPage.propTypes = {
-  MDXContent: PropTypes.string.isRequired,
+const CustomPage = ({ MDXContent, page, errorCode }) => {
+  if (errorCode) return <AnError page={page} statusCode={errorCode} />
+
+  return (
+    <MDXProvider components={components}>
+      <div>
+        <p>
+          Hello{" "}
+          <Link href="/p2">
+            <a>Earth</a>
+          </Link>
+        </p>
+        <MDXRuntime>{MDXContent}</MDXRuntime>
+        <p>
+          <Link href={`/custom/ed/${page}`}>
+            <a>Edit</a>
+          </Link>
+        </p>
+      </div>
+    </MDXProvider>
+  )
 }
 
-FrontPage.getInitialProps = async (o) => {
+CustomPage.propTypes = {
+  MDXContent: PropTypes.string.isRequired,
+  page: PropTypes.string.isRequired,
+}
+
+CustomPage.getInitialProps = async (o) => {
   const {
     req,
+    res,
     query: { page },
   } = o
   console.log(!req, typeof req, page)
-  const res = await fetch(`http://localhost:3000/${page}.mdx`)
-  const c = await res.text()
+  const res2 = await fetch(`http://localhost:3000/${page}.mdx`)
+  if (!res2.ok) {
+    if (res) res.statusCode = 404
+    return {
+      errorCode: 404,
+      MDXContent: "",
+      page,
+    }
+  }
+  console.log("RES", res2.ok)
+  const c = await res2.text()
   return { MDXContent: c, page }
 }
 
-export default FrontPage
+export default CustomPage

86b4a9570cbbdada5fda0c76ed955a61132e024d
Robin Millette <robin@millette.info>
2019-09-22 13:37:09 -0400

    Touchups.

--- pages/api/c1.js
+++ pages/api/c1.js
@@ -1,11 +1,14 @@
 // core
 import fs from "fs"
 
-export default ({ method, body }, res) => {
+const ApiC1 = ({ method, body }, res) => {
   if (method !== "POST")
     return res.status(405).json({ error: "Method Not Allowed", method })
-  fs.writeFile("public/c1.mdx", body.cnt, "utf8", (error, a) => {
-    if (error) return res.status(500).json({ error })
-    res.status(200).json({ ok: true, title: "Next.js", a })
+  const { cnt } = body
+  fs.writeFile("public/c1.mdx", cnt, "utf8", (error) => {
+    if (error) return res.status(500).json({ ...error, message: error.message })
+    res.json({ ok: true, size: cnt.length })
   })
 }
+
+export default ApiC1

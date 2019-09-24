b9b036089d885f7793dd875f928c067018a9f12a
Robin Millette <robin@millette.info>
2019-09-24 19:19:27 -0400

    Add API route to enumerate custom pages.

---
+++ pages/api/customs.js
@@ -0,0 +1,21 @@
+// core
+import { readdir } from "fs"
+
+const isMdx = (source) => source.split(".").slice(-1)[0] === "mdx"
+
+const output = (source) => ({
+  source,
+  href: `/custom/${source.slice(0, -4)}`,
+})
+
+const ApiCustoms = ({ method }, res) => {
+  if (method !== "GET")
+    return res.status(405).json({ error: "Method Not Allowed", method })
+
+  readdir("public/custom/", (error, files) => {
+    if (error) return res.status(500).json({ error })
+    res.json({ ok: true, pages: files.filter(isMdx).map(output) })
+  })
+}
+
+export default ApiCustoms

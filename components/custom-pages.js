f504d1f736721bf9eb16e97865ddaa90bee8eebb
Robin Millette <robin@millette.info>
2019-09-24 20:13:35 -0400

    Show list of pages (client-side only).

--- components/custom-pages.js
+++ components/custom-pages.js
@@ -1,5 +1,29 @@
-const CustomPages = (props) => {
-  return <div>List of Custom Pages....</div>
+// npm
+import "isomorphic-unfetch"
+import { useState, useEffect } from "react"
+
+const CustomPages = () => {
+  const [pages, setPages] = useState([])
+
+  useEffect(async () => {
+    const res2 = await fetch(`http://localhost:3000/api/customs`)
+    const pages = await res2.json()
+    setPages(pages)
+  }, [])
+
+  return (
+    <div>
+      <h3>List of Custom Pages</h3>
+      <pre>{JSON.stringify(pages, null, 2)}</pre>
+    </div>
+  )
 }
 
+/*
+CustomPages.getInitialProps = async () => {
+  const res2 = await fetch(`http://localhost:3000/api/customs`)
+  const pages = await res2.json()
+  return { pages }
+}
+*/
 export default CustomPages

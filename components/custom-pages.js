3bc6e2c32bf89e79d19a0351aaec2cb9108d4be6
Robin Millette <robin@millette.info>
2019-09-25 13:37:57 -0400

    Healthier CustomPages component.

--- components/custom-pages.js
+++ components/custom-pages.js
@@ -1,21 +1,23 @@
 // npm
 import Link from "next/link"
 
-const CustomPages = ({ pages }) => () => (
-  <div>
-    <h3>List of Custom Pages</h3>
-    <ul>
-      {pages &&
-        pages.length > 0 &&
-        pages.map(({ source, href }) => (
-          <li key={source}>
-            <Link href="/custom/[page]" as={href}>
-              <a>{source}</a>
-            </Link>
-          </li>
-        ))}
-    </ul>
-  </div>
-)
-
-export default CustomPages
+export default ({ pages }) => {
+  const CustomPages = () => (
+    <div>
+      <h3>List of Custom Pages</h3>
+      <ul>
+        {pages &&
+          pages.length > 0 &&
+          pages.map(({ source, href }) => (
+            <li key={source}>
+              <Link href="/custom/[page]" as={href}>
+                <a>{source}</a>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+  CustomPages.displayName = "custom-pages-component"
+  return CustomPages
+}

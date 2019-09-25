35ef85da815c4b297bf9a97af0536aea8744121b
Robin Millette <robin@millette.info>
2019-09-25 13:07:47 -0400

    Touchups.

--- pages/api/ed/[page].js
+++ pages/api/ed/[page].js
@@ -1,19 +1,19 @@
 // core
-import fs from "fs"
-import stream from "stream"
+import { open, writeFile, createReadStream } from "fs"
+import { pipeline } from "stream"
 
 const ApiCustomPage = ({ method, body, query: { page } }, res) => {
   if (method !== "POST" && method !== "GET")
     return res.status(405).json({ error: "Method Not Allowed", method })
   if (method === "GET") {
-    fs.open(`docs/custom/${page}.mdx`, (error, fd) => {
+    open(`docs/custom/${page}.mdx`, (error, fd) => {
       if (error)
         return res
           .status(error.code === "ENOENT" ? 404 : 500)
           .json({ ...error, message: error.message })
-      res.setHeader("Content-Type", "text/plain")
-      stream.pipeline(
-        fs.createReadStream("", { encoding: "utf8", fd }),
+      res.setHeader("Content-Type", "text/plain") // text/mdx is the official mime type
+      pipeline(
+        createReadStream("", { encoding: "utf8", fd }),
         res,
         (error) =>
           error && res.status(500).json({ ...error, message: error.message })
@@ -21,7 +21,7 @@ const ApiCustomPage = ({ method, body, query: { page } }, res) => {
     })
   } else {
     const { cnt } = body
-    fs.writeFile(`docs/custom/${page}.mdx`, cnt, "utf8", (error) => {
+    writeFile(`docs/custom/${page}.mdx`, cnt, "utf8", (error) => {
       if (error)
         return res.status(500).json({ ...error, message: error.message })
       res.json({ ok: true, size: cnt.length })

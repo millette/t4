b900168e5efd3e899af27f019f9e6864a977fa77
Robin Millette <robin@millette.info>
2019-09-25 12:57:21 -0400

    API route to get custom docs.

--- pages/api/ed/[page].js
+++ pages/api/ed/[page].js
@@ -1,14 +1,32 @@
 // core
 import fs from "fs"
+import stream from "stream"
 
-const ApiC1 = ({ method, body, query: { page } }, res) => {
-  if (method !== "POST")
+const ApiCustomPage = ({ method, body, query: { page } }, res) => {
+  if (method !== "POST" && method !== "GET")
     return res.status(405).json({ error: "Method Not Allowed", method })
-  const { cnt } = body
-  fs.writeFile(`public/customs/${page}.mdx`, cnt, "utf8", (error) => {
-    if (error) return res.status(500).json({ ...error, message: error.message })
-    res.json({ ok: true, size: cnt.length })
-  })
+  if (method === "GET") {
+    fs.open(`docs/custom/${page}.mdx`, (error, fd) => {
+      if (error)
+        return res
+          .status(error.code === "ENOENT" ? 404 : 500)
+          .json({ ...error, message: error.message })
+      res.setHeader("Content-Type", "text/plain")
+      stream.pipeline(
+        fs.createReadStream("", { encoding: "utf8", fd }),
+        res,
+        (error) =>
+          error && res.status(500).json({ ...error, message: error.message })
+      )
+    })
+  } else {
+    const { cnt } = body
+    fs.writeFile(`docs/custom/${page}.mdx`, cnt, "utf8", (error) => {
+      if (error)
+        return res.status(500).json({ ...error, message: error.message })
+      res.json({ ok: true, size: cnt.length })
+    })
+  }
 }
 
-export default ApiC1
+export default ApiCustomPage

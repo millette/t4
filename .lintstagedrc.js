88016f24b437a6156dd7b820e09d799b903cdad7
Robin Millette <robin@millette.info>
2019-09-26 17:56:00 -0400

    Debug lint-staged...

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,9 +3,11 @@
 const quote = (f) => `'${f.replace(/\[/g, "\\[").replace(/\]/g, "\\]")}'`
 
 module.exports = {
-  "*.{js,json,md,mdx}": (filenames) =>
-    filenames.reduce((commands, filename) => {
+  "*.{js,json,md,mdx}": (filenames) => {
+    console.log("FILENAMES", filenames)
+    return filenames.reduce((commands, filename) => {
       const fn = quote(filename)
       return [...commands, `prettier --write ${fn}`, `git add ${fn}`]
-    }, []),
+    }, [])
+  },
 }

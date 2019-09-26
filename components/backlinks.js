9b7429e3ba5d336b56d8a1a5312f4e9b3b09ae02
Robin Millette <robin@millette.info>
2019-09-25 20:46:14 -0400

    Link actual backlinks.

--- components/backlinks.js
+++ components/backlinks.js
@@ -1,44 +1,45 @@
 // npm
+import Link from "next/link"
 import { useState, useEffect } from "react"
-// import PropTypes from "prop-types"
 import { useRouter } from "next/router"
 import "isomorphic-unfetch"
 
-const Backlinks = (props) => {
-  const router = useRouter()
-
-  const [diff, setDiff] = useState()
+const Backlinks = () => {
+  const {
+    query: { page },
+  } = useRouter()
+  const [backlinks, setBacklinks] = useState()
 
   useEffect(() => {
-    if (!router.query.page) return
-    fetch(`/api/backlinks/${router.query.page}`).then(async (res) => {
-      if (!res.ok) return
+    fetch(`/api/backlinks/${page}`).then(async (res) => {
+      if (!res.ok) return setBacklinks([])
       const j = await res.json()
-      setDiff(j)
+      setBacklinks(j)
     })
   }, [])
 
-  /*
-  <h3>props</h3>
-  <pre>{JSON.stringify(props, null, 2)}</pre>
-  <h3>router</h3>
-  <pre>{JSON.stringify(router, null, 2)}</pre>
-  */
+  // <pre>{JSON.stringify(backlinks, null, 2)}</pre>
+  // <pre>{JSON.stringify(b, null, 2)}</pre>
 
   return (
     <div>
-      <h3>diff</h3>
-      {diff ? (
-        <pre>{JSON.stringify(diff, null, 2)}</pre>
+      <h3>Pages linking here</h3>
+      {backlinks ? (
+        <ul>
+          {backlinks.map((b) => (
+            <li key={b.from}>
+              <Link href="/custom/[page]" as={`/custom/${b.from}`}>
+                <a>{b.from}</a>
+              </Link>{" "}
+              with text "{b.text}" {b.title && `and title "${b.title}"`}
+            </li>
+          ))}
+        </ul>
       ) : (
         <div>Loading backlinks...</div>
       )}
     </div>
   )
-}
-
-Backlinks.propTypes = {
-  // kind: PropTypes.string,
 }
 
 export default Backlinks

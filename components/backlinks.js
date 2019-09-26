028960a64c419444c5d6d58d6b71fc8f911550e6
Robin Millette <robin@millette.info>
2019-09-25 20:09:33 -0400

    Load backlinks, client-side.

--- components/backlinks.js
+++ components/backlinks.js
@@ -1,26 +1,38 @@
 // npm
-// import { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 // import PropTypes from "prop-types"
 import { useRouter } from "next/router"
-// import "isomorphic-unfetch"
+import "isomorphic-unfetch"
 
 const Backlinks = (props) => {
   const router = useRouter()
 
-  /*
-  const [diff, setDiff] = useState(0)
+  const [diff, setDiff] = useState()
 
   useEffect(() => {
-    // setInterval(() => setT1(Date.now()), interval)
+    if (!router.query.page) return
+    fetch(`/api/backlinks/${router.query.page}`).then(async (res) => {
+      if (!res.ok) return
+      const j = await res.json()
+      setDiff(j)
+    })
   }, [])
+
+  /*
+  <h3>props</h3>
+  <pre>{JSON.stringify(props, null, 2)}</pre>
+  <h3>router</h3>
+  <pre>{JSON.stringify(router, null, 2)}</pre>
   */
 
   return (
     <div>
-      <h3>props</h3>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
-      <h3>router</h3>
-      <pre>{JSON.stringify(router, null, 2)}</pre>
+      <h3>diff</h3>
+      {diff ? (
+        <pre>{JSON.stringify(diff, null, 2)}</pre>
+      ) : (
+        <div>Loading backlinks...</div>
+      )}
     </div>
   )
 }

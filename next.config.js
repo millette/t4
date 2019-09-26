1635cf68f45c8c288b76f62b677d7e1ad8603d70
Robin Millette <robin@millette.info>
2019-09-25 22:19:32 -0400

    Bundle analyzer.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+// npm
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
+module.exports = withBundleAnalyzer({
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) config.node = { fs: "empty" }
     return config
   },
-}
+})

ba8411298b222650b08fd98d883f1334662081ba
Robin Millette <robin@millette.info>
2019-09-26 17:59:48 -0400

    Stick with lint-staged 9.3.0.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,6 @@ const quote = (f) => `'${f.replace(/\[/g, "\\[").replace(/\]/g, "\\]")}'`
 
 module.exports = {
   "*.{js,json,md,mdx}": (filenames) => {
-    console.log("FILENAMES", filenames)
     return filenames.reduce((commands, filename) => {
       const fn = quote(filename)
       return [...commands, `prettier --write ${fn}`, `git add ${fn}`]

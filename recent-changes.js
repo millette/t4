524a1398ecffd06ade070319cc32bc2fd72f6148
Robin Millette <robin@millette.info>
2019-09-28 20:17:18 -0400

    File watcher over docs/custom/ directory.

---
+++ recent-changes.js
@@ -0,0 +1,14 @@
+const watch = require("node-watch")
+
+const changes = (em) => {
+  watch("docs/custom", (evt, name) => {
+    if (name.split(".").slice(-1)[0] === "mdx")
+      em.emit("recent-changes", {
+        evt,
+        name: name.slice(12),
+        date: new Date().toISOString(),
+      })
+  })
+}
+
+module.exports = changes
